e266d98d978d9c00d861d85607756148f1221e35
Marcos Puente <jmpuente@gmail.com>
2017-04-26 17:57:41 -0500

    Se agrega la funcionalidad del echo

--- main.c
+++ main.c
@@ -1,15 +1,33 @@
 #define uC18f4620
 #include <18F4620.h>
-#fuses XT, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT//,BORV28
-#fuses NOPBADEN, MCLR, STVREN, NOLVP, NODEBUG, 
+#fuses XT, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
+#fuses NOPBADEN, MCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 
+#define RX_232        PIN_C7
+#define TX_232        PIN_C6
+#use RS232(BAUD=9600, XMIT=TX_232, RCV=RX_232, BITS=8,PARITY=N, STOP=1)
 
+
+char caracterRecibido;
+byte banderas0;
+#bit banderaNuevoCaracter = banderas0.0
+
+#int_rda
+void isr_rda(){
+   while(kbhit()){
+     caracterRecibido=getc();
+     banderaNuevoCaracter=1;
+   }
+}   
 void main (void){
-	setup_oscillator(OSC_16MHZ | OSC_NORMAL   );
-	set_tris_D(0x00);
-	while(1){
-		output_toggle(PIN_D0);
-		delay_ms(1);
-	}
+   setup_oscillator(OSC_16MHZ | OSC_NORMAL   );
+   set_tris_c(0x80);
+   banderaNuevoCaracter=0;
+   while(1){
+      if(banderaNuevoCaracter==1){
+         banderaNuevoCaracter=0;
+         putc(caracterRecibido);
+      }
+   }
 }		
