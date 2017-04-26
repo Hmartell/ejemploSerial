5b5ab9a37dc23ffa05070c8b6eb345d01de0f225
Marcos Puente <jmpuente@gmail.com>
2017-04-26 18:16:21 -0500

    Funcionalidad echo terminada

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #define TX_232        PIN_C6
 #use RS232(BAUD=9600, XMIT=TX_232, RCV=RX_232, BITS=8,PARITY=N, STOP=1)
 
-
+#use fast_io(c)
 char caracterRecibido;
 byte banderas0;
 #bit banderaNuevoCaracter = banderas0.0
@@ -24,6 +24,10 @@ void main (void){
    setup_oscillator(OSC_16MHZ | OSC_NORMAL   );
    set_tris_c(0x80);
    banderaNuevoCaracter=0;
+   clear_interrupt(INT_RDA);
+   enable_interrupts(INT_RDA);
+   enable_interrupts(GLOBAL);
+   printf("Hola Mundo\n");
    while(1){
       if(banderaNuevoCaracter==1){
          banderaNuevoCaracter=0;
